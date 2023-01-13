d96cd1cd81e8a4e74a816d78b1dcb9870cea36e7
0xMartin <martin.krcma1@gmail.com>
2023-01-13 01:15:50 +0100

    update

--- app/src/main/java/cz/utb/photostudio/CameraFragment.kt
+++ app/src/main/java/cz/utb/photostudio/CameraFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import cz.utb.photostudio.databinding.FragmentFirstBinding
+import cz.utb.photostudio.databinding.FragmentCameraBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class CameraFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCameraBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +32,11 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /*
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+         */
     }
 
     override fun onDestroyView() {
--- app/src/main/java/cz/utb/photostudio/SecondFragment.kt
+++ app/src/main/java/cz/utb/photostudio/SecondFragment.kt
@@ -32,9 +32,11 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /*
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+         */
     }
 
     override fun onDestroyView() {
